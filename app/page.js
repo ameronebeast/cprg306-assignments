f727bfa5ee4821ab858d4fb1d0732412393c2a3b
ameronebeast <140218691+ameronebeast@users.noreply.github.com>
2024-03-19 11:08:19 +0530

    BABY COME NAAA

--- app/page.js
+++ app/page.js
@@ -17,6 +17,8 @@ export default function Home() {
         </Link>
         <Link href="/week-7">Week 7
         </Link>
+        <Link href="/week-8">Week 8
+        </Link>
         
       </p>
     </main>
